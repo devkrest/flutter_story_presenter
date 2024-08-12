d895cf840ead0877488d1949e20eff5533410717
Harsh <40534244+harshlet@users.noreply.github.com>
2024-08-12 20:27:22 +0530

    :feature: Audio Story - just audio added - audio view started - audio config started

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,17 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import flutter_inappwebview_macos
+import just_audio
 import path_provider_foundation
 import sqflite
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
---
+++ lib/src/utils/audio_player.dart
@@ -0,0 +1,3 @@
+import 'package:just_audio/just_audio.dart';
+
+final audioPlayer = AudioPlayer();
